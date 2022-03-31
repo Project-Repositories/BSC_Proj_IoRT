5299e3d84df611f25370646eadc819f75447c9ae
WorldZage <augustfkv@hotmail.com>
2022-03-31 20:19:55 +0200

    soft adjustment of direction added again

--- my_driving_code/Line_Tracking.py
+++ my_driving_code/Line_Tracking.py
@@ -22,21 +22,20 @@ class Line_Tracking:
         # 3: (4000, 4000, -2000, -2000) turn soft left
         # 4: (-1500, -1500, 2500, 2500) turn sharp right
         # 6: (-2000, -2000, 4000, 4000) turn soft left
-        action_dict = {1: (1500, 1500, -500, -500), 2: (800, 800, 800, 800),
-                       3: (4000, 4000, -2000, -2000), 4: (-500, -500, 1500, 1500),
-                       6: (-2000, -2000, 4000, 4000)}
+        action_dict = {1: (800, 800, -400, -400), 2: (600, 600, 600, 600),
+                       3: (1000, 1000, -500, -500), 4: (-400, -400, 800, 800),
+                       6: (-500, -500, 1000, 1000)}
 
         while True:
-            le_mi_ri = 0  # left middle right
-            if GPIO.input(self.IR01):
+            le_mi_ri = 0  # accumulating value for the three infrared sensors.
+            if GPIO.input(self.IR01):  # Left
                 le_mi_ri += 4
-            if GPIO.input(self.IR02):
+            if GPIO.input(self.IR02):  # Middle
                 le_mi_ri += 2
-            if GPIO.input(self.IR03):
+            if GPIO.input(self.IR03):  # Right
                 le_mi_ri += 1
             motor_values = action_dict.get(le_mi_ri, (600, 600, 600, 600))
-            # temporary:
-            motor_values = (600, 600, 600, 600)
+            # motor_values = (600, 600, 600, 600)
             if le_mi_ri in (0, 5, 7) and time_to_change:
                 direction *= -1  # invert direction
                 time_to_change = False
@@ -44,7 +43,7 @@ class Line_Tracking:
                 time_to_change = True
             motor_values = [direction * num for num in motor_values]
             PWM.setMotorModel(*motor_values)
-            time.sleep(0.35)
+            time.sleep(0.5)
 
 
 infrared = Line_Tracking()
