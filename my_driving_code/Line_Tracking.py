3da021e919cc4b86ff79955c856f6d3b8cbf6f22
WorldZage <augustfkv@hotmail.com>
2022-03-31 23:24:46 +0200

    testing default line tracking implementation

--- my_driving_code/Line_Tracking.py
+++ my_driving_code/Line_Tracking.py
@@ -58,12 +58,35 @@ class Line_Tracking:
             time.sleep(0.05 + (0.1 * (direction == -1)))  # time to drive before checking again
             # + (0.1 * can_inverse)
 
+    def run2(self):
+        while True:
+            self.LMR = 0x00
+            if GPIO.input(self.IR01) == True:
+                self.LMR = (self.LMR | 4)
+            if GPIO.input(self.IR02) == True:
+                self.LMR = (self.LMR | 2)
+            if GPIO.input(self.IR03) == True:
+                self.LMR = (self.LMR | 1)
+            if self.LMR == 2:
+                PWM.setMotorModel(800, 800, 800, 800)
+            elif self.LMR == 4:
+                PWM.setMotorModel(-1500, -1500, 2500, 2500)
+            elif self.LMR == 6:
+                PWM.setMotorModel(-2000, -2000, 4000, 4000)
+            elif self.LMR == 1:
+                PWM.setMotorModel(2500, 2500, -1500, -1500)
+            elif self.LMR == 3:
+                PWM.setMotorModel(4000, 4000, -2000, -2000)
+            elif self.LMR == 7:
+                # pass
+                PWM.setMotorModel(0, 0, 0, 0)
+
 
 infrared = Line_Tracking()
 # Main program logic follows:
 if __name__ == '__main__':
     print('Program is starting ... ')
     try:
-        infrared.run()
+        infrared.run2()
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program  will be  executed.
         PWM.setMotorModel(0, 0, 0, 0)
