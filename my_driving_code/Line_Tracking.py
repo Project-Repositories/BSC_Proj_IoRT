30366dca81662cc6ae1e19fc6eddf6c05bfa3ca9
WorldZage <augustfkv@hotmail.com>
2022-03-31 23:32:09 +0200

    test default linetrack implementation 2

--- my_driving_code/Line_Tracking.py
+++ my_driving_code/Line_Tracking.py
@@ -60,24 +60,24 @@ class Line_Tracking:
 
     def run2(self):
         while True:
-            self.LMR = 0x00
-            if GPIO.input(self.IR01) == True:
-                self.LMR = (self.LMR | 4)
-            if GPIO.input(self.IR02) == True:
-                self.LMR = (self.LMR | 2)
-            if GPIO.input(self.IR03) == True:
-                self.LMR = (self.LMR | 1)
-            if self.LMR == 2:
+            le_mi_ri = 0  # accumulating value for the three infrared sensors.
+            if GPIO.input(self.IR01):  # Left
+                le_mi_ri += 4
+            if GPIO.input(self.IR02):  # Middle
+                le_mi_ri += 2
+            if GPIO.input(self.IR03):  # Right
+                le_mi_ri += 1
+            if le_mi_ri == 2:
                 PWM.setMotorModel(800, 800, 800, 800)
-            elif self.LMR == 4:
+            elif le_mi_ri == 4:
                 PWM.setMotorModel(-1500, -1500, 2500, 2500)
-            elif self.LMR == 6:
+            elif le_mi_ri == 6:
                 PWM.setMotorModel(-2000, -2000, 4000, 4000)
-            elif self.LMR == 1:
+            elif le_mi_ri == 1:
                 PWM.setMotorModel(2500, 2500, -1500, -1500)
-            elif self.LMR == 3:
+            elif le_mi_ri == 3:
                 PWM.setMotorModel(4000, 4000, -2000, -2000)
-            elif self.LMR == 7:
+            elif le_mi_ri == 7:
                 # pass
                 PWM.setMotorModel(0, 0, 0, 0)
 
