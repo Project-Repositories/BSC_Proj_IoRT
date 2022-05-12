c833c8e0c9dde019cfeddca26929d570b26b2c81
August <augustfkv@hotmail.com>
2022-05-12 09:23:12 +0200

    linetracker debug 2

--- my_driving_code/line_tracker.py
+++ my_driving_code/line_tracker.py
@@ -41,6 +41,15 @@ class LineTracker:
             else:  # Drive forward
                 self.recent_track = Tracking.FORWARD
                 # PWM.setMotorModel(800, 800, 800, 800)
+        if (left and right) or not (left and mid and right):  # If no line detected, or left and right detected,
+            # use the most recent instruction instead.
+            pass
+        elif left:  # Turn slightly left
+            self.recent_track = Tracking.LEFT1
+            PWM.setMotorModel(-1500, -1500, 2500, 2500)
+        elif right:  # Turn slightly right
+            self.recent_track = Tracking.RIGHT1
+            # PWM.setMotorModel(2500, 2500, -1500, -1500)
 
         return self.recent_track
 
@@ -58,4 +67,3 @@ if __name__ == '__main__':
         PWM.setMotorModel(0, 0, 0, 0)
         Servo().setServoPwm('0', 90)
         Servo().setServoPwm('1', 90)
-        
